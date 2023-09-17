f365fe20d8a5a733669037327eac4d8884b2bb5d
Heinrich Kruger <heindsight@kruger.dev>
2023-09-17 15:45:11 +0100

    Improve configurator tests

--- spec/lua/pylsp_venv_spec/configurators/jedi_spec.lua
+++ spec/lua/pylsp_venv_spec/configurators/jedi_spec.lua
@@ -1,29 +1,41 @@
 describe("jedi configurator", function()
-    local jedi_configurator = require("pylsp_venv.configurators.jedi")
+    local configurator, venv
 
-    local venv = {
-        name = "test_venv",
-        path = "/foo/test_venv",
-        python_exe = "/foo/test_venv/bin/python",
-    }
-
-    it("identifies the pylsp plugin", function()
-        assert.are.equal("jedi", jedi_configurator.plugin)
+    setup(function()
+        configurator = require "pylsp_venv.configurators.jedi"
     end)
 
-    it("configures jedi environment", function()
+    teardown(function()
+        configurator = nil
+    end)
+
+    before_each(function()
+        venv = {
+            name = "test_venv",
+            path = "/foo/test_venv",
+            python_exe = "/foo/test_venv/bin/python",
+        }
+    end)
+
+    it("SHOULD identify plugin as jedi", function()
+        assert.are.equal("jedi", configurator.plugin)
+    end)
+
+    it("SHOULD configure jedi environment", function()
         local config = {}
-        jedi_configurator.configure_venv(config, venv)
+        configurator.configure_venv(config, venv)
         assert.are.same({ environment = "/foo/test_venv" }, config)
     end)
 
-    it("does nothing if environment is already set", function()
+    it("SHOULD not override existing environment setting", function()
         local config = { environment = "/some/venv" }
-        jedi_configurator.configure_venv(config, venv)
+        configurator.configure_venv(config, venv)
         assert.are.same({ environment = "/some/venv" }, config)
     end)
 
-    it("validates venv", function()
-        assert.has_error(function() jedi_configurator.configure_venv({}, { name = "incomplete" }) end)
+    it("SHOULD error on invalid venv parameter", function()
+        assert.has_error(function()
+            configurator.configure_venv({}, { name = "incomplete" })
+        end)
     end)
 end)
--- spec/lua/pylsp_venv_spec/configurators/pylsp_mypy_spec.lua
+++ spec/lua/pylsp_venv_spec/configurators/pylsp_mypy_spec.lua
@@ -1,50 +1,53 @@
 describe("pylsp_mypy configurator", function()
-    local pylsp_mypy_configurator = require("pylsp_venv.configurators.pylsp_mypy")
+    local venv, configurator
 
-    local venv = {
-        name = "test_venv",
-        path = "/foo/test_venv",
-        python_exe = "/foo/test_venv/bin/python",
-    }
-
-    it("identifies the pylsp plugin", function()
-        assert.are.equal("pylsp_mypy", pylsp_mypy_configurator.plugin)
+    setup(function()
+        configurator = require "pylsp_venv.configurators.pylsp_mypy"
     end)
 
-    it("configures pylsp_mypy python executable", function()
+    teardown(function()
+        configurator = nil
+    end)
+
+    before_each(function()
+        venv = {
+            name = "test_venv",
+            path = "/foo/test_venv",
+            python_exe = "/foo/test_venv/bin/python",
+        }
+    end)
+
+    it("SHOULD identify the plugin as pylsp_mypy", function()
+        assert.are.equal("pylsp_mypy", configurator.plugin)
+    end)
+
+    it("SHOULD configure mypy python executable", function()
         local config = {}
-        pylsp_mypy_configurator.configure_venv(config, venv)
-        assert.are.same(
-            { overrides = { true, "--python-executable", "/foo/test_venv/bin/python" } },
-            config
-        )
+        configurator.configure_venv(config, venv)
+        assert.are.same({ overrides = { true, "--python-executable", "/foo/test_venv/bin/python" } }, config)
     end)
 
-    it("preserves existing overrides", function()
+    it("SHOULD preserve existing overrides", function()
         local config = { overrides = { "--ignore-missing-imports" } }
-        pylsp_mypy_configurator.configure_venv(config, venv)
-        assert.are.same(
-            {
-                overrides = {
-                    "--ignore-missing-imports",
-                    "--python-executable",
-                    "/foo/test_venv/bin/python"
-                }
+        configurator.configure_venv(config, venv)
+        assert.are.same({
+            overrides = {
+                "--ignore-missing-imports",
+                "--python-executable",
+                "/foo/test_venv/bin/python",
             },
-            config
-        )
+        }, config)
     end)
 
-    it("does nothing if python executable is already set", function()
+    it("SHOULD not override existing python executable", function()
         local config = { overrides = { "--python-executable", "/some/venv/bin/python" } }
-        pylsp_mypy_configurator.configure_venv(config, venv)
-        assert.are.same(
-            { overrides = { "--python-executable", "/some/venv/bin/python" } },
-            config
-        )
+        configurator.configure_venv(config, venv)
+        assert.are.same({ overrides = { "--python-executable", "/some/venv/bin/python" } }, config)
     end)
 
-    it("validates venv", function()
-        assert.has_error(function() pylsp_mypy_configurator.configure_venv({}, { name = "incomplete" }) end)
+    it("SHOULD error on invalid venv parameter", function()
+        assert.has_error(function()
+            configurator.configure_venv({}, { name = "incomplete" })
+        end)
     end)
 end)
